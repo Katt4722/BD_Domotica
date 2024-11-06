ee323dd65b0f46d0ccc24ab7c6aa1841415fd1d4
Katt4722 <karen.mejiaet12d1@gmail.com>
2024-11-06 17:00:52 -0300

    cambios

--- Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ff833ded55e659ccb2c72def88c09418a52940")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+391752e0af2f22f3f022cabdcbe6895783fda94e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
